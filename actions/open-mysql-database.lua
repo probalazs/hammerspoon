581fc78146a197aa2675557e6999331e25646058
probalazs <probalazs@nyenyere.eu>
2021-08-29 13:40:27 +0200

    refactor(open-mysql-database): refactor open mysql database

--- actions/open-mysql-database.lua
+++ actions/open-mysql-database.lua
@@ -8,27 +8,44 @@ module.choice = {
     ["action"] = "OPEN_MYSQL_DATABASE"
 }
 
-function module.run()
+local getAllDatabases = function()
     local output = cache.getCacheOr("databases", function()
         return hs.execute("ssh -t devmgmt1 '/usr/local/sbin/database.sh -l'",
                           true)
     end)
-    local choices = {}
-    for line in output:gmatch("([^\n]*)\n?") do
-        if libs.startsWith(line, "  ") then
-            table.insert(choices,
-                         {["text"] = line:gsub("^%s*(.-)%(.*%)%s*$", "%1")})
-        end
+    local databases = {}
+    for database in output:gmatch("([^\n]*)\n?") do
+        table.insert(databases, database)
     end
-    libs.showDailog(choices, function(choice)
-        hs.osascript.applescript([[
-          tell application "]] .. config.terminal .. [["
-            create window with default profile command "]] ..
-                                     "ssh -t devmgmt1 '/usr/local/sbin/database.sh " ..
-                                     choice.text .. "'" .. [["
-          end tell
-        ]])
+    return databases
+end
+
+local getFilteredDatabases = function(databases)
+    return hs.fnutils.filter(databases, function(database)
+        return libs.startsWith(database, "  ")
     end)
+end
+
+local getDatabaseChoices = function(databases)
+    return hs.fnutils.map(databases, function(database)
+        return {["text"] = database:gsub("^%s*(.-)%(.*%)%s*$", "%1")}
+    end)
+end
+
+local openTerminal = function(database)
+    hs.osascript.applescript([[
+    tell application "]] .. config.terminal .. [["
+      create window with default profile command "]] ..
+                                 "ssh -t devmgmt1 '/usr/local/sbin/database.sh " ..
+                                 database .. "'" .. [["
+    end tell
+  ]])
+end
+
+function module.run()
+    local filteredDatabases = getFilteredDatabases(getAllDatabases())
+    libs.showDailog(getDatabaseChoices(filteredDatabases),
+                    function(choice) openTerminal(choice.text) end)
 end
 
 return module
