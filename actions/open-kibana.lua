9dc6298e6aebe24b8c905c0e7e0038eb482eeabc
probalazs <probalazs@nyenyere.eu>
2021-08-29 13:17:00 +0200

    refactor(open-kibana-secrets): refactor open kibana

--- actions/open-kibana.lua
+++ actions/open-kibana.lua
@@ -2,7 +2,7 @@ local module = {}
 
 module.choice = {["text"] = "Kibana", ["action"] = "OPEN_KIBANA"}
 
-function module.run()
+local getIndexes = function()
     local response = cache.getCacheOr("kibana", function()
         local _, data = hs.http.post(
                             "http://eslb.emar.sys:9200/kibana-int/dashboard/_search",
@@ -12,12 +12,17 @@ function module.run()
             }), {["Accept"] = "application/json"})
         return data
     end)
-    local choices = {}
-    for _, index in pairs(hs.json.decode(response).hits.hits) do
-        table.insert(choices,
-                     {["text"] = index._id, ["id"] = libs.urlencode(index._id)})
-    end
-    libs.showDailog(choices, function(choice)
+    return hs.json.decode(response).hits.hits
+end
+
+local getIndexChoices = function(indexes)
+    return hs.fnutils.map(indexes, function(index)
+        return {["text"] = index._id, ["id"] = libs.urlencode(index._id)}
+    end)
+end
+
+function module.run()
+    libs.showDailog(getIndexChoices(getIndexes()), function(choice)
         hs.urlevent.openURL(
             "http://kibana.emar.sys/#/dashboard/elasticsearch/" .. choice.id)
     end)
