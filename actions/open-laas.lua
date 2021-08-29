fd3da7bec6c36bb08f23b4609a6aa972e59c128f
probalazs <probalazs@nyenyere.eu>
2021-08-29 13:20:15 +0200

    refactor(open-laas): refactor open laas

--- actions/open-laas.lua
+++ actions/open-laas.lua
@@ -4,19 +4,27 @@ local module = {}
 
 module.choice = {["text"] = "Laas", ["action"] = "OPEN_LAAS"}
 
-function module.run()
+local getIndexes = function()
     local response = cache.getCacheOr("laas", function()
         local _, data = hs.http.get(
                             "https://laas-kibana.service.emarsys.net/api/saved_objects/?type=index-pattern&fields=title&per_page=10000",
                             {["Accept"] = "application/json"})
         return data
     end)
-    local choices = {}
-    for _, index in pairs(hs.json.decode(response).saved_objects) do
-        table.insert(choices,
-                     {["text"] = index.attributes.title, ["id"] = index.id})
-    end
-    libs.showDailog(choices, function(choice)
+    return hs.json.decode(response).saved_objects
+end
+
+local getIndexChoices = function(indexes)
+    return hs.fnutils.map(indexes, function(index)
+        return {
+            ["text"] = index.attributes.title,
+            ["id"] = libs.urlencode(index.id)
+        }
+    end)
+end
+
+function module.run()
+    libs.showDailog(getIndexChoices(getIndexes()), function(choice)
         hs.urlevent.openURL(
             "https://laas-kibana.service.emarsys.net/app/kibana#/discover?_g=()&_a=(columns:!(_source),index:" ..
                 choice.id ..
