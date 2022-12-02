8978fe3bb5622ccbcd84a9151c077718059c6cf3
probalazs <probalazs@nyenyere.eu>
2022-12-02 15:50:52 +0100

    feat(actions): remove unused cache handling

--- actions/clear-cache.lua
+++
@@ -1,9 +0,0 @@
-cache = require('../cache')
-
-local module = {}
-
-module.choice = { ['text'] = 'Clear cache', ['action'] = 'CLEAR_CACHE' }
-
-function module.run() cache.clear() end
-
-return module
