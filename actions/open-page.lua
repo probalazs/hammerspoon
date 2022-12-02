10ac8d115060572241e89c7fb26691f05b8ceca3
probalazs <probalazs@nyenyere.eu>
2022-12-02 15:51:03 +0100

    fix(actions): open page

--- actions/open-page.lua
+++ actions/open-page.lua
@@ -2,8 +2,6 @@ config = require('../config')
 
 local module = {}
 
-function module.run()
-  function module.run() libs.showDailog(config.pages, function(choice) hs.urlevent.openURL(choice.url) end) end
-end
+function module.run() libs.showDailog(config.pages, function(choice) hs.urlevent.openURL(choice.url) end) end
 
 return module
