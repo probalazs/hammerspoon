a1168858e8f173621f16dbfd9b04a6de911bb8e8
probalazs <probalazs@nyenyere.eu>
2022-12-02 11:59:14 +0100

    docs(app): add example config

--- config.example.lua
+++ config.example.lua
@@ -1,7 +1,10 @@
 local config = {
   vpn = { username = 'user', connectionName = 'emarsys-remote-access' },
   pages = { { text = 'Telex', url = 'https://telex.hu' } },
-  meetingRooms = { { text = 'First room', id = '123' }, { text = 'Second room', id = '456' } }
+  meetingRooms = {
+    { text = 'First room', id = '123' },
+    { text = 'Second room', id = '456' }
+  }
 }
 
 return config
