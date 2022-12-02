63ed9a89cc280973de346109b96126b7e2ee1f42
probalazs <probalazs@nyenyere.eu>
2022-12-02 14:51:35 +0100

    feat(actions): use seal

--- actions/go-to-meeting.lua
+++ actions/go-to-meeting.lua
@@ -1,11 +1,11 @@
-config = require("../config")
+config = require('../config')
 
 local module = {}
 
 function module.run()
-    libs.showDailog(config.meetingRooms, function(choice)
-        hs.urlevent.openURL("https://emarsys.zoom.us/j/" .. choice.id)
-    end)
+  libs.showDailog(
+      config.meetingRooms, function(choice) hs.urlevent.openURL('https://emarsys.zoom.us/j/' .. choice.id) end
+  )
 end
 
 return module
--- init.lua
+++ init.lua
@@ -17,33 +17,26 @@ spoon.SpoonInstall:andUse(
       start = true
     }
 )
-
-local actions = {
-  { text = 'Open page', module = openPage },
-  { text = 'Go to meeting', module = goToMeeting },
-  { text = 'Connect emarsys vpn', module = connectToVpn },
-  { text = 'Start work', module = startWork },
-  { text = 'Stop work', module = stopWork },
-  { text = 'Reload hammerspoon config', module = reloadHammerspoon },
-  { text = 'Restart WiFi', module = restartWifi },
-}
-
-local getChoices = function(actions)
-  local choices = {}
-  for _, action in pairs(actions) do
-    table.insert(choices, { text = action.text })
-  end
-  return choices;
-end
-
-hs.hotkey.bind(
-    { 'cmd', 'alt', 'ctrl' }, 'P',
-        function() libs.showDailog(getChoices(actions), function(choice)
-            for _, action in pairs(actions) do
-                if action.text == choice.text then
-                    action.module.run()
-                end
-            end
-        end)
-    end
+spoon.SpoonInstall:andUse(
+    'Seal', {
+      hotkeys = { show = { { 'cmd', 'alt', 'ctrl' }, 'space' } },
+      fn = function(seal)
+        seal:loadPlugins({ 'apps', 'calc', 'useractions' })
+        seal.plugins.apps.appSearchPaths = {
+          '/Applications', '/System/Applications', '~/Applications', '/Developer/Applications'
+        }
+        seal.plugins.useractions.actions = {
+          ['Open page'] = { fn = openPage.run },
+          ['Go to meeting'] = { fn = goToMeeting.run },
+          ['Connect emarsys vpn'] = { fn = connectToVpn.run },
+          ['Start work'] = { fn = startWork.run },
+          ['Stop work'] = { fn = stopWork.run },
+          ['Reload hammerspoon config'] = { fn = reloadHammerspoon.run },
+          ['Restart WiFi'] = { fn = restartWifi.run }
+        }
+        seal:refreshAllCommands()
+        seal.plugins.apps:restart()
+      end,
+      start = true
+    }
 )
